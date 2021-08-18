0744d1114caa66a32dff0667ad84cf6ee807df55
Michael Jones <m.pricejones@gmail.com>
2021-08-18 17:09:25 +0100

    Add build of site

---
+++ docs/precache-manifest.ae169dd5112280cfa4e4f1a5e6f290ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "3c1364c1d813f76ab8592492a402acb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "7816fb0923fc42c7bbff",
+    "url": "/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "7816fb0923fc42c7bbff",
+    "url": "/static/js/main.7816fb09.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
